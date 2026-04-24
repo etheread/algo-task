258ace14224cb312229b6ff7f812a759a27197d4
etheread <provladychenko@gmail.com>
2026-04-24 13:23:06 +0300

    learned params search

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,16 @@
-import { Controller,Get } from '@nestjs/common';
+import { Controller,Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   @Get()
-  vladickMode():object{
-    return this.usersService.vladickMode()
+  private vladickMode():object{
+    return this.usersService.users()
+  }
+  @Get('/:id')
+  findById(@Param('id') id:string) {
+    return this.usersService.findById(Number(id))
+    
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,28 @@
-import { Get, Injectable } from '@nestjs/common';
+import { Get, Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-    vladickMode():object {
-        return {
+    users() {
+        return [{
             id:1,
             title:'vladick brudskoe'
-        }
+        },
+    {
+        id:2,
+        title:'pidoras'
+    },
+    {
+        id:3,
+        title:'huesos'        
+    }]
     }
+
+    findById(id:number) {
+        const user = this.users().find((user) => user.id === id)
+        if (!user) {
+            throw new NotFoundException('no user with taht name')
+        }
+        return user
+    }
+    
 }
