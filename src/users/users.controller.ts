0ec3210549cd1f861f66477ab919d847e3e8b034
etheread <provladychenko@gmail.com>
2026-04-25 15:25:16 +0300

    added post request

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,21 @@
-import { Controller,Get, Param } from '@nestjs/common';
+import { Body, Controller,Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   @Get()
   private vladickMode():object{
-    return this.usersService.users()
+    return this.usersService.getAll()
   }
   @Get('/:id')
   findById(@Param('id') id:string) {
     return this.usersService.findById(Number(id))
     
+  }
+  @Post()
+  createNewUser(@Body() dto:CreateUserDto) {
+    return this.usersService.createNew(dto)
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,28 +1,44 @@
-import { Get, Injectable, NotFoundException } from '@nestjs/common';
+import { Body, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create.dto';
 
 @Injectable()
 export class UsersService {
-    users() {
-        return [{
+    private users = [
+        {
             id:1,
             title:'vladick brudskoe'
         },
-    {
-        id:2,
-        title:'pidoras'
-    },
-    {
-        id:3,
-        title:'huesos'        
-    }]
-    }
+        {
+           id:2,
+            title:'pidoras'
+        },
+        {
+            id:3,
+            title:'huesos'        
+        }
+    ]
 
     findById(id:number) {
-        const user = this.users().find((user) => user.id === id)
+        const user = this.users.find((user) => user.id === id)
         if (!user) {
             throw new NotFoundException('no user with taht name')
         }
         return user
+    }
+
+    getAll() {
+        return this.users
+    }
+
+
+    createNew(dto:CreateUserDto) {
+        const newUser = {
+        id:this.users.length + 1,
+        title:dto.title
+    };
+    this.users.push(newUser)
+
+        return this.users
     }
     
 }
