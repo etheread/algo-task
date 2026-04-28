c1ccb38ab1622d4ac6cae3aa735958d567549215
etheread <provladychenko@gmail.com>
2026-04-28 22:47:41 +0300

    made see all users and see users by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User } from './entity/users.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  @Get()
+    findAll () {
+     return this.usersService.getAll()
+    }
+
+    @Get(':id')
+    getById(@Param('id') id:number) {
+      return this.getById(id)
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entity/users.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 
 export class UsersService {
-    
+    constructor(
+    @InjectRepository(User)
+    private userRepo:Repository<User>
+    ){}
+    getAll() {
+        if (!this.userRepo) {
+            return 'sosal'
+        }
+        return this.userRepo.find()
+    }
+
+    getByParam(id:number) {
+       const user =  this.userRepo.find({where:{
+        id:id
+       }})
+
+       if(!user) {
+        return
+       }
+        return this.userRepo
+    }
 }
