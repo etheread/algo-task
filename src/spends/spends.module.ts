e89f2b7bc24e2c5644ff60d91acfb42313e141ea
etheread <provladychenko@gmail.com>
2026-04-30 20:10:09 +0300

    added entity and imported it to module

--- src/spends/spends.module.ts
+++ src/spends/spends.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SpendsService } from './spends.service';
 import { SpendsController } from './spends.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Spend } from './entity/spends.entity';
 
 @Module({
   controllers: [SpendsController],
   providers: [SpendsService],
+  imports:[TypeOrmModule.forFeature([Spend])]
 })
 export class SpendsModule {}
