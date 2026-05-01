ba4fea7fe821a2b75e729ae8c6414e90fb08057e
etheread <provladychenko@gmail.com>
2026-05-01 20:06:11 +0300

    added entity and dto for user spends

---
+++ src/spends/dto/createSpend.dto.ts
@@ -0,0 +1,15 @@
+import { IsNumber,IsString } from "class-validator";
+
+export class CreateSpendDto {
+    @IsNumber()
+    id!:number
+
+    @IsString()
+    spend!:string
+
+    @IsString()
+    spendCategory!:string
+
+    @IsString()
+    spendPrice!:string
+}
